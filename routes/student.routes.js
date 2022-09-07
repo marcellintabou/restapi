8984280334edab874e48527f77a483379e85a382
marcellin <marcellintabou@gmail.com>
2022-09-07 11:40:32 -0400

    First commiting !

---
+++ routes/student.routes.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const studentExpressRoute = express.Router();
+let StudentSchema = require('../model/student.model');
+
+studentExpressRoute.route('/').get((req, res) => {
+    StudentSchema.find((error, data) => {
+        if(error){
+            return next(error);
+        }else{
+            res.json(data);
+        }
+    })
+})
+
+studentExpressRoute.route('/:id').get((req, res) => {
+    StudentSchema.findById(req.params.id, (error, data) => {
+        if(error){
+            return next(error);
+        }else{
+            res.json(data);
+        }
+    })
+})
+
+studentExpressRoute.route('/').post((req, res, next) => {
+    //console.log(req.route);
+    StudentSchema.create(req.body, (error, data) => {
+        if(error){
+            return next(error);
+        }else{
+            res.status(200).json({
+                msg: data
+            });
+        }
+    })
+})
+
+studentExpressRoute.route('/:id').delete((req, res) => {
+    //console.log(req.route);
+    StudentSchema.findByIdAndRemove(req.params.id, (error, data) => {
+        if(error){
+            return next(error);
+        }else{
+            res.status(200).json({
+                msg: data
+            });
+        }
+    })
+})
+
+studentExpressRoute.route('/:id').put((req, res) => {
+    StudentSchema.findByIdAndUpdate(req.params.id, {$set: req.body}, (error, data) => {
+            if(error){
+                return next(error);
+            }else{
+                res.json(data);
+            }
+        }
+    )
+})
+
+function handleResponse(error, res) {
+    if(error){
+        return next(error);
+    }else{
+        res.json(data);
+    }
+}
+module.exports = studentExpressRoute;
---
+++ server.js
@@ -0,0 +1,48 @@
+let express = require('express');
+const createError = require('http-errors'); 
+path = require('path');
+mongoose = require('mongoose');
+cors = require('cors');
+bodyParser = require('body-parser');
+dbConfig = require('./db/database');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true
+}).then(
+    () => {
+        console.log("Database connected !!!")
+    }, 
+    error => {
+        console.log("Database could not be connected : " + error)
+    }
+)
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(cors());
+
+const studentsRoute = require('./routes/student.routes')
+
+app.use('/students', studentsRoute);
+
+const port = process.env.PORT || 8080;
+
+const server = app.listen(port, () => {
+    console.log('Port connected to: ' + port);
+})
+
+app.use(function(req, res, next) {
+    next(createError(404, 'Not Found'))
+})
+
+app.get('/', (req, res) => {
+    res.send("invalid endpoint");
+})
+app.use(function(req, res, next) {
+    if(!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+})
+
